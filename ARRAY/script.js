509b27b1e868e8d4cb4628e2f83266b48c9c48eb
amandeep-singh-parihar <amandeepparihar24@gmail.com>
2024-05-13 10:33:14 +0530

    array function on going

---
+++ ARRAY/script.js
@@ -0,0 +1,29 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// let arr1 = new Array();
+// arr1[0] = 1;
+// arr1[1] = 2;
+// arr1[3] = 10;
+// console.log(arr1);
+
+// let ans = arr.find((a) => {
+//   return a == 8;
+// });
+
+// arr.splice(2,3,-100000,-99999);
+
+// console.log(arr);
+
+// console.log(ans);
+
+// let ans = arr.map((n) => {
+//   return n + 10;
+// });
+
+// let naam = ["aman", "deep", "singh", "paihar", "abhay", "rauthan"];
+// let res = naam.map((naam) => {
+//   return naam.toUpperCase();
+// });
+
+let ans = arr.forEach((ele) => {
+  console.log(ele * 100);
+});
